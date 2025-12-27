314d4545be9e54c412d685c5d2963c2c9e8bdff7
Rohit <rrohit2911@gmail.com>
2025-12-27 14:57:20 +0530

    API hidden

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDs6COexDv30MaIiWGo-7RIrDDmxDkuxOg",
+  apiKey: import.meta.env.VITE_FIREBASE_API,
   authDomain: "smart-civic-eye-83e60.firebaseapp.com",
   projectId: "smart-civic-eye-83e60",
   storageBucket: "smart-civic-eye-83e60.firebasestorage.app",
