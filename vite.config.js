c15b01b199ade9734c859886954fb8bfd14ec71b
Rohit <rrohit2911@gmail.com>
2025-12-31 08:56:16 +0530

    PWA done

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,56 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'; 
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+  base: "./",
   server : {
     allowedHosts : ['.ngrok-free.app']
-  }
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate', // Updates app immediately when you deploy new changes
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Smart Civic Eye',
+        short_name: 'CivicEye',
+        description: 'AI-Powered Citizen Reporting Agent',
+        theme_color: '#2563EB', // Your Electric Blue
+        background_color: '#ffffff',
+        display: 'standalone', // Makes it look like a native app (no browser bar)
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          // {
+          //   src: '/pwa-512x512.png',
+          //   sizes: '512x512',
+          //   type: 'image/png',
+          //   purpose: 'any'
+          // },
+          // {
+          //   src: '/pwa-maskable-192x192.png',
+          //   sizes: '192x192',
+          //   type: 'image/png',
+          //   purpose: 'maskable'
+          // },
+          // {
+          //   src: '/pwa-maskable-512x512.png',
+          //   sizes: '512x512',
+          //   type: 'image/png',
+          //   purpose: 'maskable'
+          // }
+        ]
+      }
+    })
+  ]
 })
